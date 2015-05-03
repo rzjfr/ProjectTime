a4737df7ef99242607839bfcbb2c3d780df163e8
rzjfr <rzjfr@yahoo.com>
2015-05-04 01:49:54 +0430

    fix datepicker load problem

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
     $('.datepicker').datepicker({format: 'yyyy-mm-dd'});
 });
+$(document).on('page:load', function(){
+    $('.datepicker').datepicker({format: 'yyyy-mm-dd'});
+});
