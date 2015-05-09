c3f95fd963eb5057fa4311a3ee984cbf3602594f
rzjfr <rzjfr@yahoo.com>
2015-05-09 14:43:03 +0430

    scheduling send done task to archive

--- config/initializers/rufus_scheduler.rb
+++ config/initializers/rufus_scheduler.rb
@@ -1,13 +1,19 @@
 SCHEDULER.every("1h") do
-  Milestone.all.each do |milestone|
-    first_id = Project.find(milestone.project_id).first_milestone.id
-    current_id = Project.find(milestone.project_id).current_milestone.id
-    if milestone.passed? && (milestone.id != first_id)
-      if !milestone.task.empty?
-        puts "All tasks for " + milestone.full_title + " assigned to "
-        Task.where(milestone_id: milestone.id, state: "Backlog").update_all(milestone_id: first_id)
-        Task.where(milestone_id: milestone.id, state: "Progress").update_all(milestone_id: current_id)
-        Task.where(milestone_id: milestone.id, state: "Done").update_all(milestone_id: current_id)
+  Project.all.each do |project|
+    next if !project.uses_milestones?
+    Project.milestone.all.each do |milestone|
+      first_id = project.first_milestone.id
+      current_id = project.current_milestone.id
+      if milestone.passed? && (milestone.id != first_id)
+        if !milestone.task.empty?
+            puts "All tasks for " + milestone.full_title + " assigned to "
+            Task.where(milestone_id: milestone.id,
+                       state: "Backlog").update_all(milestone_id: first_id)
+            Task.where(milestone_id: milestone.id, state:
+                       "Progress").update_all(milestone_id: current_id)
+            Task.where(milestone_id: milestone.id,
+                       state: "Done").update_all(state: "Archived")
+        end
       end
     end
   end
